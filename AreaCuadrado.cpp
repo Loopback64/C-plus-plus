df459db3f375cceb0f84518d1694770ce4085551
loopback64 <luishever@gmail.com>
2024-01-17 23:53:18 -0500

    Programa que calcula el área de un cuadrado

---
+++ AreaCuadrado.cpp
@@ -0,0 +1,36 @@
+
+
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // Declarar variables
+    double lado, area;
+
+    // Solicitar al usuario la longitud de un lado
+    cout << "Ingrese la longitud de un lado del cuadrado: ";
+    cin >> lado;
+
+    // Calcular el área del cuadrado
+    area = lado * lado;
+
+    // Mostrar el resultado
+    cout << "El área del cuadrado es: " << area << endl;
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
