8c7f00eff982fe4237bab5537c1f45d41846a556
loopback64 <luishever@gmail.com>
2024-01-20 01:02:40 -0500

    Como calcular el area de un triangulo

---
+++ AreaTriangulo.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+int main() {
+
+	// Declarar variables para la base y la altura
+	double base, altura;
+		
+	// Solicitar al usuario que ingrese la base
+	std::cout << "Ingrese la longitud de la base del triangulo: ";
+	std::cin >> base;
+	
+	// Solicitar al usuario que ingrese la altura
+	std::cout << "Ingrese la altura del triangulo: ";
+	std::cin >> altura;
+	// Calcular el área del triángulo
+	double area = 0.5 * base * altura;
+		
+	// Mostrar el resultado
+	std::cout << "El area del triangulo es: " << area << std::endl;
+		
+	return 0;
+	}
+		
