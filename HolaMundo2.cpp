701ed72fc1c3aedd40cdee1d11e5a41537fe544c
loopback64 <luishever@gmail.com>
2024-01-21 23:51:45 -0500

    Compilanod mi primer Hola Mundo en C++ con el comando linux g++ -o HolaMundo2 HolaMundo2.cpp

---
+++ HolaMundo2.cpp
@@ -0,0 +1,31 @@
+
+
+#include "iostream"
+
+using namespace std;
+
+int main()
+
+{
+
+cout << "Hola Mundo\n"; 
+
+//system("PAUSE"); //Solo poner sino te da error
+
+
+return 0;
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
