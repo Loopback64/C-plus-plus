cebfcd61291a41bc5b7b6ff1812d9b1aeb94efd7
loopback64 <luishever@gmail.com>
2024-02-16 19:14:23 -0500

    Agregando nueva variable  base2

--- AreaTriangulo.cpp
+++ AreaTriangulo.cpp
@@ -3,7 +3,7 @@
 int main() {
 
 	// Declarar variables para la base y la altura
-	double base, altura;
+	double base, altura, base2;
 		
 	// Solicitar al usuario que ingrese la base
 	std::cout << "Ingrese la longitud de la base del triangulo: ";
@@ -12,6 +12,7 @@ int main() {
 	// Solicitar al usuario que ingrese la altura
 	std::cout << "Ingrese la altura del triangulo: ";
 	std::cin >> altura;
+	std::cin >> base2;
 	// Calcular el área del triángulo
 	double area = 0.5 * base * altura;
 		
@@ -20,4 +21,7 @@ int main() {
 		
 	return 0;
 	}
+
+
+
 		
