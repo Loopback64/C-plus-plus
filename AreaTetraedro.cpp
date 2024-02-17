4989929fdd4c01223af235121e6432788d8bd842
loopback64 <luishever@gmail.com>
2024-02-17 16:17:38 -0500

    Calculando el área de un tetraedro regular

---
+++ AreaTetraedro.cpp
@@ -0,0 +1,114 @@
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+
+// Función para calcular el área lateral del Tetraedro 
+//
+double calcularAreaLateral(double longitudArista) {
+
+	double areaTriangulo = ( sqrt(3)/4 * pow(longitudArista,2);
+	return 3 * areaTriangulo;
+
+
+} 
+
+int main()      {
+
+	double longitudArista;
+	
+	cout <<  "Ingrese la longitud de una arista del tetraedro: "; 
+	cin <<   longitudArista; 
+
+
+
+	double areaLateral = calcularAreaLateral( longitudArista);
+	
+	cout << "El área lateral del tetraedro es : " << areaLateral << ednl;
+
+	return 0;
+
+}	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	
+	
+	
+	
+	
+	
+	
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	
+
+	
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
